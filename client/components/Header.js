73e051ce8511ecfb030fb9f41a3f65627dec9d94
Sean M <sean@arrowburn.co.nz>
2018-06-29 11:16:07 +1200

    added ability to find out whether we're logged in or not

--- client/components/Header.js
+++ client/components/Header.js
@@ -4,12 +4,24 @@ import query from '../queries/CurrentUser';
 
 
 class Header extends Component {
+    renderButtons() {
+        const { loading, user } = this.props.data;
+        if (loading) { return <div />; }
+        if (user) {
+            return <div>Logout</div>;
+        } else {
+            return (
+                <div>You're not signed in</div>
+            )
+        }
+    }
     render() {
-        console.log(this.props.data);
         return(
-            <div>
-                Header 
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    {this.renderButtons()}
+                </div>
+            </nav>
         )
     }
 }
